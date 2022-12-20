ed657a6e6470981d9ec67f2ddeed5d56f0e2aa4a
Smarsden007 <seannmarsden@gmail.com>
2022-12-19 21:26:06 -0800

    Working on styling

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -1,26 +1,23 @@
-import React, { useState,useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const TodoForm = () => {
-  const [description, setDescription] = useState('');
-  const [postedBy, setPostedBy] = useState('');
-  const [employeeData, setEmployeeData] = useState(null);
+  const [description, setDescription] = useState("");
+  const [postedBy, setPostedBy] = useState("");
+  const [employeeData, setEmployeeData] = useState("");
 
   useEffect(() => {
     async function fetchEmployeeData() {
-      const res = await axios.get('/api/employees');
-      console.log(res.data)
+      const res = await axios.get("http://localhost:5000/api/employees");
+      console.log(res.data);
       setEmployeeData(res.data);
     }
     fetchEmployeeData();
   }, []);
-
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-
     try {
-      const res = await axios.post('http://localhost:5000/api/tasks', {
+      const res = await axios.post("http://localhost:5000/api/tasks", {
         description,
         postedBy,
       });
@@ -31,26 +28,28 @@ const TodoForm = () => {
     }
   };
 
+
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
+    <form class='p-5' onSubmit={handleSubmit}>
+      <label class='text-white'>
         Description:
         <input
-          type="text"
+          type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           value={description}
-          onChange={event => setDescription(event.target.value)}
+          onChange={(event) => setDescription(event.target.value)}
         />
       </label>
       <br />
-      <label>
+      <label class='text-white'>
         Posted By:
-         {employeeData && (
-        <select>
-          {employeeData.map(employee => (
-            <option value={employee.id}>{employee.name}</option>
-          ))}
-        </select>
-      )}
+        {employeeData && (
+          <select>
+            {employeeData.map((employee) => (
+              <option key={employee.id}>{employee.name}</option>
+            ))}
+          </select>
+        )}
       </label>
       <br />
       <button type="submit">Add Todo</button>
