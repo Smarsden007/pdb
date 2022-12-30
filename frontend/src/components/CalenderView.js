77e720a2ca9e1322acd481d7fa5913dcd1fff882
Smarsden007 <seannmarsden@gmail.com>
2022-12-29 23:29:58 -0800

    Calendar is rendering data on dates. need to add  scroll bar and pop up modal for expanded bookings view.

--- frontend/src/components/CalenderView.js
+++ frontend/src/components/CalenderView.js
@@ -1,132 +1,103 @@
 import React, { useState, useEffect } from "react";
+import Calendar from "react-calendar";
 import styled from "styled-components";
 
-const StyledDiv = styled.div`
-  height: 100px;
-  width: 100px;
-  background-color: ${(props) => (props.isRented ? "red" : "#4a5568")};
-  border-radius: 0.25rem;
-  border: 1px solid purple;
-  margin: .5rem;
-`;
-export const CalendarView = () => {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-  const [rentalDates, setRentalDates] = useState([]);
-
-  const month = months[currentMonth];
-  const year = currentYear;
-
-  const previousMonth = () => {
-    // Update the current month and year to the previous month
-    if (currentMonth === 0) {
-      setCurrentMonth(11);
-      setCurrentYear(currentYear - 1);
-    } else {
-      setCurrentMonth(currentMonth - 1);
-    }
-  };
-
-  const nextMonth = () => {
-    // Update the current month and year to the next month
-    if (currentMonth === 11) {
-      setCurrentMonth(0);
-      setCurrentYear(currentYear + 1);
-    } else {
-      setCurrentMonth(currentMonth + 1);
-    }
-  };
-
-  // Use an effect hook to fetch the rental dates from the database when the component mounts
-  // or when the current month or year changes
-  useEffect(() => {
-    async function fetchRentalDates() {
-      const response = await fetch(
-        `http://localhost:5000/api/rental_dates?month=${currentMonth}&year=${currentYear}`
-      );
-      const rentalDates = await response.json();
-      console.log("tessss", rentalDates);
-      const rentalDatesObjects = rentalDates.map(
-        (rentalDate) => new Date(rentalDate.rental_date)
-      );
-      setRentalDates(rentalDatesObjects);
-      console.log(rentalDates);
-    }
-
-    fetchRentalDates();
-  }, [currentMonth, currentYear]);
-
-  // Get the number of days in the current month
-  const daysInMonth = new Date(year, currentMonth + 1, 0).getDate();
-
-  // Generate an array of days for the current month
-  const days = [...Array(daysInMonth).keys()].map((i) => i + 1);
-
-  // Split the days into weeks (each week is an array of 7 days)
-  const weeks = [];
-  while (days.length > 0) {
-    weeks.push(days.splice(0, 7));
+const StyledCalendar = styled(Calendar)`
+  
+  .react-calendar__tile {
+    width: 6rem;
+    height: 10rem;
+    text-align: left;
+    border: 1px solid #a0a096;
+    border-radius: .25rem;
+    padding: .25rem;
+    margin: .45rem;
   }
+  .react-calendar__navigation__arrow {
+    font-size: 1.5rem;
+    color: pink;
+    padding: 2rem;
+  }
+  .react-calendar__day-number {
+    color: red;
+    font-weight: bold;
+    text-align: left;
+  }
+  .data-tile {
+    background-color: red;
+  }
+ 
+  /* .react-calendar__tile--active {
+  background: #006edc;
+  color: white;
+} */
 
+.react-calendar__month-view__weekdays__weekday {
+    font-size: 16px;
+    color: rgb(15, 70, 15);
+}
+.react-calendar__month-view__weekNumbers {
+  font-weight: bold;
+
+}
+.react-calendar--selectRange .react-calendar__tile--hover {
+  background-color: #e6e6e6;
+}
+`;
+
+
+export const CalendarView = () => {
+  const [bookings, setBookings] = useState([]);
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    // Fetch the bookings data from the back-end server
+    fetch("http://localhost:5000/api/bookings")
+      .then((response) => response.json())
+      .then(({ bookings }) => {
+        console.log(bookings);
+        setBookings(bookings);
+        console.log(bookings, "aslodjalosdjhdsh");
+      });
+  }, []);
+
+  const handleChange = (date) => {
+    setDate(date);
+  };
+ 
+  
   return (
-    <div class="mb-10 mt-8 ml-44">
-      <div class=' grid grid-cols-3 w-3/5 ml-16 justify-center	'>
-        <button class="outline rounded bg-gradient-to-r from-purple-500 to-pink-500 p-2 ml-3 mb-3 text-xs text-white  hover:text-black m-5" onClick={previousMonth}>Previous</button>
-        <span class='font-extrabold	 p-2 mb-3 m-5 justify-center	'>
-          {month} {year}
-        </span>
-        <button class="outline rounded bg-gradient-to-r from-purple-500 to-pink-500 p-2 ml-3 mb-3 text-xs text-white hover:text-black m-5" onClick={nextMonth}>Next</button>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
-          </tr>
-        </thead>
-        <tbody >
-          {weeks.map((week, index) => (
-            <tr key={index}>
-              {week.map((day, index) => {
-            const isRented = rentalDates.some((rentalDate) => {
-              const date = new Date(Date.parse(rentalDate.rental_date));
-              return (
-                date.getMonth() === currentMonth &&
-                date.getFullYear() === currentYear &&
-                date.getDate() === day
-              );
-            });
-            
-            
-            return (
-              <td class='hover:scale-110 m-8' key={day}>
-              <StyledDiv  isRented={isRented}><p class='p-2 text-white bg-gradient-to-r from-purple-500 to-pink-500 h-8'>{day}</p></StyledDiv></td>
-            );
-            
+    <div>
+    <div >
+    <StyledCalendar
+      onChange={handleChange}
+      value={date}
+      tileContent={({ date, view }) => {
+        const formattedDate = date.toISOString().slice(0, 10).replace(/-/g, "");
+        const bookingsOnDate = bookings.filter(
+          (booking) =>
+            booking.rent_date.slice(0, 10).replace(/-/g, "") === formattedDate
+        );
+      
+        console.log(bookingsOnDate);
+        return bookingsOnDate.length > 0 ? (
+            <ul className="text-left text-xs">
+              {bookingsOnDate.map((booking) => {
+               
+                return (
+                  <li key={booking.id}>
+                    {booking.fulll_name} - Paid: {booking.paid ? "Yes" : "No"}  Bouncer: {booking.bouncer}
+                  </li>
+                );
               })}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            </ul>
+          ) : null;
+          
+      }
+    }      
+    />
+    </div>
     </div>
   );
+  
 };
--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -7,13 +7,15 @@ import { CalendarView } from "../components/CalenderView";
 export const Calendar = ()=> {
   
   return (
-    <div class="grid grid-cols-10 grid-rows-2 overflow-hidden	">
+    <div class="grid grid-cols-10 grid-rows-2 overflow-hidden">
 
-      <div class="col-span-2 row-span-2">
+      <div class="col-span-2 row-span-2 overflow-hidden">
         <Navbar />
       </div>
       <div class='col-span-8 row-span-2 overflow-hidden	' >
+        <div class="overflow-hidden">
         <CalendarView />
+        </div>
            </div>
     </div>
   );
