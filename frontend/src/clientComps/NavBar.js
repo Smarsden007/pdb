1bd162352c7a1d841a01b242f663030ff7e12931
Smarsden007 <seannmarsden@gmail.com>
2023-01-17 00:48:05 -0800

    Wokring on final homepage responsive grid

--- frontend/src/clientComps/NavBar.js
+++ frontend/src/clientComps/NavBar.js
@@ -5,13 +5,14 @@ export const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div>
+    <div class={`${isOpen ? "fixed top-0 left-0 w-full h-full bg-white z-50" : ""}`}>
+
       <div class="fixed top-0 z-10 bg-transparent w-full">
         <nav class="container mx-auto flex justify-end">
           <div class="block lg:hidden ">
             <button
               onClick={() => setIsOpen(!isOpen)}
-              class="flex items-center m-5 px-3 py-2 border rounded text-beige-200 border-stone-500 hover:text-red-300 hover:border-red-300"
+              class="flex items-center m-2 px-5 py-3 border rounded text-beige-200 border-stone-500 hover:text-red-300 hover:border-red-300"
             >
               <svg
                 class="fill-current h-3 w-3"
@@ -29,8 +30,20 @@ export const NavBar = () => {
           <ul
             class={`text-right font-light text-2xl p-4 inline-flex ${
               isOpen ? "" : "hidden lg:flex justify-end mx-22"
-            }`}
+            } lg:hidden`}
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+              backgroundColor: "white",
+              zIndex: "5",
+              display: isOpen ? "block" : "none",
+            }}
           >
+            <div className="grid grid-cols-2">
+              <div className="gird col-start-2">
             <li class="m-4  hover:text-slate-400">
               <a href="#">HOME</a>
             </li>
@@ -47,8 +60,20 @@ export const NavBar = () => {
               <a href="#">PARTNERS</a>
             </li>
             <li class="m-4 hover:text-slate-400 ">
-             <Link to='/'> <a href="#">BOOK</a></Link>
+              <Link to="/">
+                {" "}
+                <a href="#">BOOK</a>
+                
+              </Link>
+              </li>
+              </div>
+              <div className="grid col-start-1 absolute top-0">
+              <li class="m-4 hover:text-slate-400 ">
+              <button onClick={() => setIsOpen(!isOpen)}>x</button>
             </li>
+              </div>
+              </div>
+           
           </ul>
         </nav>
       </div>
--- frontend/src/clientPages/HomePage2.js
+++ frontend/src/clientPages/HomePage2.js
@@ -7,30 +7,36 @@ import { Button } from "antd";
 export const HomePage = () => {
   return (
     <div>
-      <div className="grid grid-cols-2 grid-rows-1 md:grid-cols-4 md:grid-rows-3 lg:grid-cols-6 lg:grid-rows-2 h-8/12">
+      <div className="grid grid-cols-4 grid-rows-1 lg:grid-cols-7 lg:grid-rows-1 h-8/12">
         {/* Left Image */}
-        <div className="h-1/2 col-span-1 row-span-1 md:col-span-2 md:row-span-2 lg:col-span-3 lg:row-span-3 lg:h5/12">
+        <div className="h-1/2 col-span-2  row-span-1  lg:col-span-3 lg:row-span-3 lg:h5/12">
           <img class="h-auto w-10/12" src={ripHero}></img>
         </div>
         {/* Middle Overlap Slogan */}
-        <div class="absolute top-60 left-96 z-10 ">
-          <img class="h-auto w-8/12 invisible md:invisible lg:visible sm:invisible" src={gravity}></img>
+        <div class="absolute top-40 left-96 z-10 ">
+          <img
+            class="h-auto w-8/12 invisible md:invisible lg:visible sm:invisible"
+            src={gravity}
+          ></img>
         </div>
         {/* Right Round Top Image */}
-        <div className=" h-1/2  lg:col-start-5 lg:col-span-2 lg:row-start-2 lg:-mt-36  md:row-start-2 md:col-start-4">
-          <img class="h-auto w-auto visible  md:visable lg:visible" src={roundTop}></img>
+        <div className=" h-1/2  lg:col-start-6 lg:col-span-2 lg:row-start-1 lg:mt-48 ">
+          <img
+            class="h-auto w-auto invisible md:invisible lg:visible sm:invisible"
+            src={roundTop}
+          ></img>
         </div>
         {/* Center Text Top Image */}
-        <div className="h-1/2 mt-14 col-start-2 row-start-1 lg:col-start-4 lg:row-start-2 lg:col-span-1 lg:mt-10 md:mt-0 lg:-ml-16 md:row-start-2 md:col-start-3">
+        <div className="flex-1 flex p-5 flex-col justify-end h-1/2 col-start-3 row-start-1 col-span-2 mt-20 md:mt-20 lg:col-start-4 lg:row-start-1 lg:col-span-2 lg:mt-80 lg:p-5  ">
           {" "}
-          <h1 className="text-sm lg:text-lg p-2">
+          <h1 className="text-xs lg:text-lg p-2">
             All the Lorem Ipsum generators on the Internet tend to repeat
             predefined chunks as necessary, making this the first true generator
             on the Internet. It uses a dictionary of over 200 Latin words,
             combined with a handful of model sentence structures, to generate
             Lorem Ipsum which looks reasonable.
           </h1>
-          <Button className="mt-2 lg:mt-6">View Rentals</Button>
+          <Button className="mt-2 lg:mt-6 w-48">View Rentals</Button>
         </div>
       </div>
     </div>
