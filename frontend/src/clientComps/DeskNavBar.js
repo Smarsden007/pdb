68aced8bbe0a63d19f393f20a829a10d8f523eb0
Smarsden007 <seannmarsden@gmail.com>
2023-01-18 00:43:28 -0800

    Minor changes

--- frontend/src/clientComps/DeskNavBar.js
+++ frontend/src/clientComps/DeskNavBar.js
@@ -4,7 +4,7 @@ export const DeskNavBar = () => {
 
   return (
     <div>
-    <nav class="fixed top-0 right-0 w-1/2 h-12 text-[1.25rem] bg-white  py-1 z-50">
+    <nav class="fixed top-0 right-0 w-1/2 h-12 text-[1.25rem] lg:mt-4 bg-white  py-1 z-50">
       <ul class="flex items-center">
         <li class="m-3  hover:text-slate-400">
           <a href="#">HOME</a>
--- frontend/src/clientPages/HomePage2.js
+++ frontend/src/clientPages/HomePage2.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ripHero from "./../Media/LeftHeroRip.png";
+import ripHero from "./../Media/LeftHeroRip2.png";
 import gravity from "./../Media/Test.png";
 import roundTop from "./../Media/RoundTop.png";
 import Castle from "./../Media/CastlePol.png";
@@ -17,9 +17,9 @@ export const HomePage = () => {
           <img src={Castle} alt="image3" class="w-2/3 max-w-xs" />
         </div>
       </div> */}
-      <div className="grid grid-cols-4 grid-rows-1 lg:grid-cols-7 lg:grid-rows-1 h-8/12">
+      <div className="grid grid-cols-4 grid-rows-1 lg:grid-cols-9 lg:grid-rows-1 h-8/12">
         {/* Left Image */}
-        <div className="h-1/2 col-span-2  row-span-1  lg:col-span-3 lg:row-span-3 lg:h-6/12">
+        <div className="object-top h-7/12 col-span-2  row-span-1  lg:col-span-4 lg:row-span-3 lg:h-6/12">
           <img class="lg:h-6/12 w-12/12" src={ripHero}></img>
         </div>
 
@@ -31,7 +31,7 @@ export const HomePage = () => {
           ></img>
         </div>
         {/* Right Round Top Image */}
-        <div className="flex-1 flex p-5 flex-col lg:justify-end h-1/2 md:col-start-3 md:row-start-1 md:col-span-2 lg:mt-[21rem] lg:col-start-6 lg:col-span-2 lg:row-start-1 lg:row-span-1 md:mt-28">
+        <div className="flex-1 flex p-5 flex-col lg:justify-end h-1/2 md:col-start-3 md:row-start-1 md:col-span-2 lg:mt-[19rem] lg:col-start-7 lg:col-span-3 lg:row-start-1 lg:row-span-2 md:mt-28">
           <img
             class="h-auto w-auto invisible md:visible lg:visible sm:invisible"
             src={roundTop}
@@ -43,7 +43,7 @@ export const HomePage = () => {
 </div>
 
         {/* Center Text Top Image */}
-        <div className="flex-1 flex p-5 mt-24 flex-col lg:justify-center md:justify-end md:h-full md:-mt-4 h-1/2 col-start-3 row-start-1 col-span-2 md:ml-20 lg:col-start-4 lg:row-start-1 lg:mt-28  lg:p-5 visible md:invisible lg:visible sm:visible">
+        <div className="flex-1 flex p-5 mt-24 flex-col lg:justify-center md:justify-end md:h-full md:-mt-4  col-start-3 row-start-1 col-span-1 md:ml-20 lg:col-start-5 lg:ml-0 lg:row-start-1 lg:mt-28  lg:p-5 visible md:invisible lg:visible sm:visible">
           <div className="md:bg-opacity-70 md:bg-white md:w-48 md:rounded-xl w-full lg:w-72 lg:bg-opacity-0">
             <h1 className="text-[.5rem] lg:text-lg p-2 md:p-4">
               All the Lorem Ipsum generators on the Internet tend to repeat
