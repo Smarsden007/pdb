d680e32f46c4ca77394f1993f27811fa2b053692
Smarsden007 <seannmarsden@gmail.com>
2023-01-21 21:42:03 -0800

    WIP Rentals Page

--- frontend/src/clientComps/RentalDetails.js
+++ frontend/src/clientComps/RentalDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './../../src/App.css'
 import {
   EditOutlined,
   EllipsisOutlined,
@@ -203,40 +204,53 @@ export const RentalDetails = () => {
         <div className="col-start-1 lg:col-start-1 lg:row-start-2 flex justify-center items-center ">
             <div
               href="#"
-              class="flex flex-col bg-white  border rounded-lg shadow-lg md:flex-row md:max-w-xl hover:scale-105"
+              class="flex flex-col bg-stone-500  border rounded-lg shadow-lg md:flex-row lg:w-11/12"
             >
               <img
                 alt="example"
                 src={Castle}
-                style={{ height: 400, borderRadius: ".5rem" }}
+                style={{ height: 400, borderRadius: ".5rem", }}
                 className=""
               />
-              <div class="flex flex-col justify-between ml-4 mr-4 leading-normal bg-white">
-                <h5 class="mb-2 p-1 text-2xl font-bold tracking-tight text-gray-900">
+              <div class="flex flex-col justify-between ml-4 mr-4 leading-normal bg-stone-050">
+                <h5 class="mb-2 p-1 text-2xl font-bold tracking-tight text-white">
                   Classic Castle
                 </h5>
-                <p class=" text-[.75rem] text-gray-700 dark:text-gray-400">
+                <p class=" text-[.75rem] text-gray-700 dark:text-white">
                   13x13
                 </p>
-                <p class=" text-[.75rem] text-gray-700 dark:text-gray-400">
+                <p class=" text-[.75rem] text-gray-700 dark:text-white">
                   4-8 Jumpers, Adult Friendly!
                 </p>
                 <Divider
                   style={{ marginBottom: ".5rem", marginTop: ".5rem" }}
                 />
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p class="mb-3 font-normal text-gray-700 dark:text-white">
                   Starting at $300.00
                 </p>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  Check out our{" "}
+                <p class="mb-3 font-normal text-gray-700 dark:text-white">
+                  Check out our
                   <Link to="/">
-                    <span className="text-stone-500"> Add-ons</span>
+                    <span className="text-grey-400"> Add-ons</span>
                   </Link>
                   !
                 </p>
+                <div className="flex flex-row">
+                            <ul className="flex flex-row">
+                            <li className=" border border-black text-black  bg-white rounded-full w-24 h-7 text-center m-1">
+                                $300/ 4 Hrs
+                              </li>
+                              <li className=" border border-black text-black  bg-white rounded-full w-24 h-7 text-center m-1">
+                              $400/ 6 Hrs
+                              </li>
+                              <li className=" border border-black text-black  bg-white rounded-full w-24 h-7 text-center m-1">
+                              $500/ 8 Hrs
+                              </li>
+                            </ul>
+                          </div>
                 <div className=" m-1">
                   <Tabs
-                    style={{ marginTop: "-1rem" }}
+                    style={{ marginTop: "-1rem", }}
                     defaultActiveKey="1"
                     items={[
                       {
@@ -245,21 +259,21 @@ export const RentalDetails = () => {
                         children: (
                           <div className="grid grid-cols-2 gap-4 h-auto">
                             <ul>
-                              <li className="border border-black rounded-full w-auto h-7 text-center m-1 p-1">
+                              <li className="border border-black text-black  bg-white rounded-full w-28 h-7 text-center m-1">
                                 Generator
                               </li>
-                              <li className="border border-black rounded-full w-auto h-7 text-center m-1 p-1">
+                              <li className="border border-black text-black  bg-white rounded-full w-28 h-7 text-center m-1">
                                 13x13 Tarp
                               </li>
-                              <li className="border border-black rounded-full w-auto h-7 text-center m-1 p-1">
+                              <li className="border border-black text-black  bg-white rounded-full w-28 h-7 text-center m-1">
                                 Shoe Rack
                               </li>
                             </ul>
                             <ul>
-                            <li className="border border-black rounded-full w-auto h-7 text-center m-1 p-1">
+                            <li className="border border-black text-black  bg-white rounded-full w-28 h-7 text-center m-1">
                                 Blower
                               </li>
-                              <li className="border border-black rounded-full w-auto h-7 text-center m-1 p-1">
+                              <li className="border border-black text-black  bg-white rounded-full w-28 h-7 text-center m-1">
                                Rules Sign
                               </li>
                             </ul>
@@ -272,14 +286,14 @@ export const RentalDetails = () => {
                         children: (
                           <div className="grid grid-cols-1 gap-4">
                             <ul>
-                              <li className="border border-black rounded-full w-36 text-center m-2 p-2">
-                                4-Hours $300
+                            <li className="border border-black text-black  bg-white rounded-full w-28 h-7 text-center m-1">
+                                Generator
                               </li>
-                              <li className="border border-black rounded-full w-36 text-center m-2 p-2">
-                                6-Hours $400
+                              <li className="border border-black text-black  bg-white rounded-full w-28 h-7 text-center m-1">
+                                Generator
                               </li>
-                              <li className="border border-black rounded-full w-36 text-center m-2 p-2">
-                                8-Hours $500
+                              <li className="border border-black text-black  bg-white rounded-full w-28 h-7 text-center m-1">
+                                Generator
                               </li>
                             </ul>
                           </div>
