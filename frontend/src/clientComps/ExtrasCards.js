3af31cc56ba711bc1084e0dd28184c211e7607c5
Smarsden007 <seannmarsden@gmail.com>
2023-02-03 23:21:42 -0800

    Working on final changes

--- frontend/src/clientComps/ExtrasCards.js
+++ frontend/src/clientComps/ExtrasCards.js
@@ -1,35 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import castle from "./../Media/CastleRentalCard.png";
+import fringe from "./../Media/fringe.png";
+import balloons from "./../Media/balloons.png";
+import vinyl from "./../Media/vinyl.png";
+import photo from "./../Media/photo1.png";
 
 export const ExtrasCards = () => {
   return (
     <div>
       <div>
         <div className="grid grid-cols-1 grid-rows-6 lg:grid-cols-3 lg:grid-rows-2 lg:gap-16 gap-8">
-            <div>
+         
           <div className="flex flex-col justify-center items-center align-center">
             <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-1">
               <div className="text-center">
                 <div className="flex flex-col p-4">
                   <h1 className="cursive-text text-4xl text-center">Vinyl</h1>
 
-                  <img alt="stars" src={castle} className="w-48 mx-auto" />
+                  <img alt="stars" src={vinyl} className="w-48 mx-auto" />
                   <div className="p-2">
                     <p className="text-bold text-center font-mono font-bold">
                       Bastille & Castle:
                     </p>
-                    <li>Large Text:$30</li>
-                    <li>Custom Theme Package:$80</li>
-                    <li>In-house Theme Package:$70</li>
+                    <li>Large Text:$40</li>
+                    <li>Full Theme Package:$75</li>
                   </div>
                   <div>
                     <p className="text-bold text-center font-mono font-bold">
                       Toddler:
                     </p>
                     <li>Large Text:$20</li>
-                    <li>Custom Theme Package:$80</li>
-                    <li>In-house Theme Package:$70</li>
+                    <li>Custom Theme Package:$55</li>
                   </div>
                 </div>
                 <Link to='/checkout'>
@@ -40,29 +42,27 @@ export const ExtrasCards = () => {
               </div>
             </div>
           </div>
-</div>
           <div className="flex flex-col justify-center items-center align-center">
-            <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-1">
+            <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-2 lg:row-start-1">
               <div className="text-center">
                 <div className="flex flex-col p-4">
                   <h1 className="cursive-text text-4xl text-center">Balloon Garland</h1>
 
-                  <img alt="stars" src={castle} className="w-48 mx-auto" />
+                  <img alt="stars" src={balloons} className="w-48 mx-auto" />
                   <div className="p-2">
                     <p className="text-bold text-center font-mono font-bold">
                       Bastille & Castle:
                     </p>
-                    <li>Large Text:$30</li>
-                    <li>Custom Theme Package:$80</li>
-                    <li>In-house Theme Package:$70</li>
+                    <li>Half Arch:$150</li>
+                    <li>Full Arch:$250</li>
+               
                   </div>
                   <div>
                     <p className="text-bold text-center font-mono font-bold">
                       Toddler:
                     </p>
-                    <li>Large Text:$20</li>
-                    <li>Custom Theme Package:$80</li>
-                    <li>In-house Theme Package:$70</li>
+                    <li>Half Arch:$100</li>
+                    <li>Full Arch:$150</li>
                   </div>
                 </div>
                 <Link to='/checkout'>
@@ -75,27 +75,25 @@ export const ExtrasCards = () => {
           </div>
 
           <div className="flex flex-col justify-center items-center align-center">
-            <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-1">
+            <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-3">
               <div className="text-center">
                 <div className="flex flex-col p-4">
                   <h1 className="cursive-text text-4xl text-center">Fringe Garland</h1>
 
-                  <img alt="stars" src={castle} className="w-48 mx-auto" />
+                  <img alt="stars" src={fringe} className="w-48 mx-auto" />
                   <div className="p-2">
                     <p className="text-bold text-center font-mono font-bold">
                       Bastille & Castle:
                     </p>
-                    <li>Large Text:$30</li>
-                    <li>Custom Theme Package:$80</li>
-                    <li>In-house Theme Package:$70</li>
+                    <li>Custom:$50</li>
+                    <li>In-house Garland:$40</li>
                   </div>
                   <div>
                     <p className="text-bold text-center font-mono font-bold">
                       Toddler:
                     </p>
-                    <li>Large Text:$20</li>
-                    <li>Custom Theme Package:$80</li>
-                    <li>In-house Theme Package:$70</li>
+                    <li>Custom:$35</li>
+                    <li>In-house Garland:$25</li>
                   </div>
                 </div>
                 <Link to='/checkout'>
@@ -113,7 +111,7 @@ export const ExtrasCards = () => {
                 <div className="flex flex-col p-4">
                   <h1 className="cursive-text text-4xl text-center">Photography</h1>
 
-                  <img alt="stars" src={castle} className="w-48 mx-auto" />
+                  <img alt="stars" src={photo} className="w-48 mx-auto" />
                   <div className="p-2">
                     <p className="text-bold text-center font-mono font-bold">
                       Bastille & Castle:
--- frontend/src/clientComps/clientStyles/RentalCards.js
+++ frontend/src/clientComps/clientStyles/RentalCards.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import castle from "./../../Media/CastleRentalCard.png";
+import toddler from "./../../Media/toddler.png";
+import bastille from "./../../Media/bastille.png";
 
 import "./RentalCards.css";
 export const RentalCards = () => {
@@ -11,7 +13,7 @@ export const RentalCards = () => {
         <div className="flex flex-col justify-center items-center align-center">
           <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-1">
             <div className="text-center">
-              <img alt="stars" src={castle} />{" "}
+              <img alt="stars" src={toddler} />{" "}
               <p className="text-bold text-center font-mono font-bold">
                 $150 4-HOURS | $250 6-HOURS
               </p>
@@ -27,7 +29,7 @@ export const RentalCards = () => {
               <p className="text-bold text-center font-mono font-bold">
                 *FOR AGES 10 AND UNDER*
               </p>
-              <Link to="/castle-checkout">
+              <Link to="/toddler-checkout">
                 <button className="bg-[#c0a58e] text-white rounded-lg w-36 h-12 hover:bg-white hover:border-[#c0a58e] hover:border-solid hover:border-4 hover:text-[#c0a58e]">
                   Book Now
                 </button>
@@ -42,7 +44,7 @@ export const RentalCards = () => {
         <div className="flex flex-col justify-center items-center align-center">
           <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e] w-72 lg:w-96 p-2 md:col-start-2 md:row-start-1 lg:col-start-2">
             <div className="text-center">
-              <img alt="stars" src={castle} />{" "}
+              <img alt="stars" src={bastille} />{" "}
               <p className="text-bold text-center font-mono font-bold">
                 $300 4-HOURS | $400 6-HOURS
               </p>
@@ -88,7 +90,7 @@ export const RentalCards = () => {
               <p className="text-bold text-center font-mono font-bold">
                 *FOR ALL AGES*
               </p>
-              <Link to="/toddler-checkout">
+              <Link to="/castle-checkout">
                 <button className="bg-[#c0a58e] text-white rounded-lg w-36 h-12 hover:bg-white hover:border-[#c0a58e] hover:border-solid hover:border-4 hover:text-[#c0a58e]">
                   Book Now
                 </button>
