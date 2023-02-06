74af296d22e58cf9f92ca15e154c527cb1ee661a
Smarsden007 <seannmarsden@gmail.com>
2023-02-05 22:31:34 -0800

    WIP

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,8 @@ require('./middlewares/passport-middleware')
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ['https://pdb-backend-production.up.railway.app','https://pdb-backend-production.up.railway.app/api/login', 'https://pouncyparties.com']
+  origin: ['https://pdb-backend-production.up.railway.app','https://pdb-backend-production.up.railway.app/api/login', 'https://pouncyparties.com'],
+  credentials: true,
 }));
 
 //{ origin: CLIENT_URL, credentials: true }
