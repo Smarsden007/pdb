1cb6c3dc247e13c67499e6e850ce8b6770b6ae2c
Smarsden007 <seannmarsden@gmail.com>
2023-03-04 23:51:50 -0800

    Changes to extras page

--- frontend/src/clientComps/ExtrasCards.js
+++ frontend/src/clientComps/ExtrasCards.js
@@ -10,10 +10,10 @@ export const ExtrasCards = () => {
   return (
     <div>
       <div>
-        <div className="grid grid-cols-1 grid-rows-5 md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 lg:gap-16 gap-8">
+        <div className="grid grid-cols-1 grid-rows-5 md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 lg:gap-16 gap-8 ">
          
-          <div className="flex flex-col justify-center items-center align-center">
-            <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-1">
+          <div className="flex flex-col justify-center items-center align-center ">
+            <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-1 ">
               <div className="text-center">
                 <div className="flex flex-col p-4">
                   <h1 className="test-font-1 text-4xl text-center">Vinyl</h1>
@@ -76,24 +76,24 @@ export const ExtrasCards = () => {
 
           <div className="flex flex-col justify-center items-center align-center">
             <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-3">
-              <div className="text-center">
+            <div className="text-center">
                 <div className="flex flex-col p-4">
-                  <h1 className="test-font-1 text-4xl text-center">Fringe Garland</h1>
+                  <h1 className="test-font-1 text-4xl text-center">Backdrops</h1>
 
-                  <img alt="stars" src={fringe} className="w-48 mx-auto" />
+                  <img alt="stars" src={backd} className="w-48 mx-auto" />
                   <div className="p-2">
                     <p className="text-bold text-center font-mono font-bold">
-                      Bastille & Castle:
+                      Trio 
                     </p>
-                    <p>Custom:$50</p>
-                    <p>In-house Garland:$40</p>
+                    <p>Solid Colors:$200 <br/>(Simple Message included)</p>
+                    <p>Complex Letters/Custom Design:Start at $300</p>
                   </div>
                   <div>
                     <p className="text-bold text-center font-mono font-bold">
-                      Toddler:
+                     Doubles
                     </p>
-                    <p>Custom:$35</p>
-                    <p>In-house Garland:$25</p>
+                    <p>Solid Colors:$150 <br/>(Simple Message included)</p>
+                    <p>Complex Letters/Custom Design:Start at $250</p>
                   </div>
                 </div>
                 <Link to='/checkout'>
@@ -105,7 +105,7 @@ export const ExtrasCards = () => {
             </div>
           </div>
 
-          <div className="flex flex-col justify-center items-center align-center">
+          {/* <div className="flex flex-col justify-center items-center align-center">
             <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-1">
               <div className="text-center">
                 <div className="flex flex-col p-4">
@@ -136,28 +136,28 @@ export const ExtrasCards = () => {
                 </Link>
               </div>
             </div>
-          </div>
+          </div> */}
 
           <div className="flex flex-col justify-center items-center align-center">
             <div className=" flex flex-col justify-center align-center items-center border-solid border-4 rounded-[4rem] border-[#c0a58e]  w-72 lg:w-96 p-2 md:col-start-1 md:row-start-1 lg:col-start-1">
-              <div className="text-center">
+            <div className="text-center">
                 <div className="flex flex-col p-4">
-                  <h1 className="test-font-1 text-4xl text-center">Backdrops</h1>
+                  <h1 className="test-font-1 text-4xl text-center">Fringe Garland</h1>
 
-                  <img alt="stars" src={backd} className="w-48 mx-auto" />
+                  <img alt="stars" src={fringe} className="w-48 mx-auto" />
                   <div className="p-2">
                     <p className="text-bold text-center font-mono font-bold">
-                      Trio 
+                      Bastille & Castle:
                     </p>
-                    <p>Solid Colors:$200 <br/>(Simple Message included)</p>
-                    <p>Complex Letters/Custom Design:Start at $300</p>
+                    <p>Custom:$50</p>
+                    <p>In-house Garland:$40</p>
                   </div>
                   <div>
                     <p className="text-bold text-center font-mono font-bold">
-                     Doubles
+                      Toddler:
                     </p>
-                    <p>Solid Colors:$150 <br/>(Simple Message included)</p>
-                    <p>Complex Letters/Custom Design:Start at $250</p>
+                    <p>Custom:$35</p>
+                    <p>In-house Garland:$25</p>
                   </div>
                 </div>
                 <Link to='/checkout'>
